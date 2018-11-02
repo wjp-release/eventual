a0a9b73fc6412745aaca154c364586b423fb806c
wjpjw <recvfromsockaddr@gmail.com>
2018-11-02 20:08:32 +0800

    w

--- src/zero_copy_value.h
+++ src/zero_copy_value.h
@@ -25,33 +25,56 @@
 
 #pragma once
 
+#include <iostream>
+
 namespace eventual{
 
 class zero_copy_value{
 public:
-    zero_copy_value();
+    // empty is a valid state
+    zero_copy_value() : meta(nullptr){}
     // none of these ctors & operators would actually make a copy
     zero_copy_value(const zero_copy_value& d) : meta(d.meta){}
+    // rvalue to be swapped as they would be obsolete anyway
     zero_copy_value& operator= (zero_copy_value&&d) noexcept : meta(d.meta){
+        std::cout<<"swap!\n";
         meta.swap(d.meta);
         return *this;
     }
+    // reference counter ++
     zero_copy_value& operator= (const zero_copy_value& d) noexcept{
+        std::cout<<"ref++\n";
         meta=d.meta;
         return *this;
     }
+    // do we refer to the same meta object?
     bool operator== (const zero_copy_value& d) noexcept{
         return meta.get()==d.meta.get();
     }
     bool operator!= (const zero_copy_value& d) const noexcept{
         return !(*this==d);
     }
+    bool empty() const noexcept{
+        return meta==nullptr;
+    }
+    // swap values between two lvalues
+    zero_copy_value& swap(zero_copy_value& d)
+    {
+        meta.swap(d.meta);
+        return this;
+    }
     // copy must be called in a very explicit way
     zero_copy_value& copy()  //return a reference to another copy of *this
     {
         return zero_copy_value(meta->copy());
     }
-
+    //
+    template<class T>
+    zero_copy_value& operator=(const T& value)
+    {
+        *this = std::move(zero_copy_value(value));
+        return *this;
+    }
 private:
     // this ctor is only used to create a copy
     zero_copy_value(std::shared_ptr<meta_t> meta):meta(meta){}
